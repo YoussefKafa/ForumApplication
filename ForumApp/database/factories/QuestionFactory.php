21d43db4d1f5f3dd64fb406cfd66e3c21386ea3d
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2021-01-03 22:59:22 +0200

    data faking and seeding

--- ForumApp/database/factories/QuestionFactory.php
+++ ForumApp/database/factories/QuestionFactory.php
@@ -2,11 +2,21 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Category;
 use App\Model\Question;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $subject = $faker->sentence;
     return [
-        //
+     'subject' => $subject,
+     'body' => $faker->text,
+     'category_id' => function(){
+         return Category::all()->random();
+     },
+     'user_id' => function(){
+         return User::all()->random();
+     }
     ];
 });
--- ForumApp/database/factories/ReplyFactory.php
+++ ForumApp/database/factories/ReplyFactory.php
@@ -2,11 +2,20 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Question;
 use App\Model\Reply;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
+    $body=$faker->text;
     return [
-        //
+        'body'=> $body,
+        'question_id'=> function(){
+            return Question::all()->random();
+        },
+        'user_id'=> function(){
+            return User::all()->random();
+        }
     ];
 });
