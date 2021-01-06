933b5e7407bf92101fe99413230236c8c9706b57
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2021-01-06 15:58:40 +0200

    intialize vue and vutify

--- ForumApp/webpack.mix.js
+++ ForumApp/webpack.mix.js
@@ -1,15 +1,24 @@
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+var VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+var webpackConfig = {
+        plugins: [
+                new VuetifyLoaderPlugin(),
+                new CaseSensitivePathsPlugin()
+                // other plugins ...
+            ]
+            // other webpack config ...
+    }
+    /*
+     |--------------------------------------------------------------------------
+     | Mix Asset Management
+     |--------------------------------------------------------------------------
+     |
+     | Mix provides a clean, fluent API for defining some Webpack build steps
+     | for your Laravel application. By default, we are compiling the Sass
+     | file for the application as well as bundling up all the JS files.
+     |
+     */
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
